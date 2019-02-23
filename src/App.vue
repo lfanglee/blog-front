77051355aad5e430059015c35dded316559aa7f2
kindboy <2515418556@qq.com>
2019-02-23 23:55:12 +0800

    feat: footer组件基本结构

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
     <div id="main" class="app-main">
         <my-header />
         <router-view></router-view>
+        <my-footer />
     </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import MyHeader from '@/components/layouts/header.vue';
+import MyFooter from '@/components/layouts/footer.vue';
 
 @Component({
     components: {
-        MyHeader
+        MyHeader,
+        MyFooter
     }
 })
 export default class App extends Vue {
---
+++ src/components/layouts/footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>footer</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class MyFooter extends Vue {
+    mounted(): void {
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
