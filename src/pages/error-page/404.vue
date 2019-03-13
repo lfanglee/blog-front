ea78733f83a73e528b7b6bd802e2f5ddd3401143
kindboy <2515418556@qq.com>
2019-03-13 21:22:01 +0800

    feat: 404错误页面

--- src/pages/error-page/404.vue
+++ src/pages/error-page/404.vue
@@ -1,9 +1,22 @@
 <template>
-    <div class="c-page-404 container">404</div>
+    <div class="c-page-404 container">
+        <img src="@/images/logo.png" alt="logo-bg" class="logo">
+        <p class="error-content">404</p>
+    </div>
 </template>
 
-<script>
-export default {
-    
-};
-</script>
+<style lang="scss" scoped>
+@import '~@/styles/tools';
+
+.c-page-404 {
+    text-align: center;
+    img {
+        width: 24rem;
+    }
+    .error-content {
+        font-size: 5rem;
+        font-weight: 700;
+        color: $black;
+    }
+}
+</style>
